6d4fd7269333d1763aef26952cb847a3cc50ce18
DongJun Park <121371412+qkrehdwns032@users.noreply.github.com>
2024-11-16 11:15:50 +0900

    jenkins test

--- src/main/java/com/example/opinionbeat/controller/IndexController.java
+++ src/main/java/com/example/opinionbeat/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.example.opinionbeat.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Date;
@@ -12,15 +9,6 @@ import java.util.Date;
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
-
-    @GetMapping("/")
-    public String index(Model model, @AuthenticationPrincipal OAuth2User oauth2User) {
-        if (oauth2User != null) {
-            String name = oauth2User.getAttribute("name");
-            model.addAttribute("userName", name);
-        }
-        return "index";  // index.html을 보여줍니다
-    }
 
     @GetMapping("/test")
     @ResponseBody
